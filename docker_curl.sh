b296dcd1f38a0524f2b7fb12a03c3e4d759cc456
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2020-02-04 15:48:06 -0500

    Added docker_curl.sh

---
+++ docker_curl.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./docker_curl.py URI"
+    exit 1
+fi
+
+URI=$1
+
+docker image remove "${USER}_bot"
+docker build --rm --tag="${USER}_bot" .
+docker run --tty --interactive --rm --name=${USER}_bot --cap-add=NET_RAW --cap-add=NET_ADMIN ${USER}_bot curl $URI
