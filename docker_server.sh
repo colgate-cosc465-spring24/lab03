007da9de11ac8f0b13157627f6757e1bac2743e2
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2020-02-27 08:45:35 -0500

    Add capture capability for server

--- docker_server.sh
+++ docker_server.sh
@@ -9,4 +9,4 @@ SERVER_PORT=$1
 
 docker image remove "${USER}_bot"
 docker build --rm --tag="${USER}_bot" .
-docker run --tty --interactive --rm --publish=$SERVER_PORT:$SERVER_PORT --name=${USER}_bot ${USER}_bot python ./server_bot.py -p $SERVER_PORT
+docker run --tty --interactive --rm --publish=$SERVER_PORT:$SERVER_PORT --name=${USER}_bot --cap-add=NET_RAW --cap-add=NET_ADMIN ${USER}_bot python ./server_bot.py -p $SERVER_PORT
